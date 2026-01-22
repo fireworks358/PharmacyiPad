4abe3445270105cfca56078f2ac2c8eef50d0ad4
fireworks358 <fireworks358@gmail.com>
2026-01-22 23:21:09 +0000

    Downgrade to Tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nhs: {
+          blue: '#005eb8',
+          'dark-blue': '#003087',
+          green: '#007f3b',
+          red: '#d5281b',
+          yellow: '#ffeb3b',
+          black: '#212b32',
+          grey: '#4c6272',
+          border: '#d8dde0',
+          background: '#f0f4f5',
+          white: '#ffffff',
+        }
+      },
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        'nhs-body': ['19px', '28px'],
+        'nhs-body-mobile': ['16px', '24px'],
+        'nhs-heading-xl': ['48px', '56px'],
+        'nhs-heading-l': ['36px', '44px'],
+        'nhs-heading-m': ['26px', '32px'],
+        'nhs-heading-s': ['22px', '28px'],
+        'nhs-display': ['80px', '88px'],
+        'nhs-display-location': ['56px', '64px'],
+      },
+      minHeight: {
+        'touch': '44px',
+      },
+      screens: {
+        'ipad': '768px',
+        'ipad-pro': '1024px',
+      },
+    },
+  },
+  plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: '/PharmacyiPad/',
-  css: {
-    transformer: 'lightningcss',
-    lightningcss: {
-      targets: {
-        safari: (12 << 16), // Safari 12.0 (iOS 12+)
-      },
-    },
-  },
-  build: {
-    cssMinify: 'lightningcss',
-  },
 })
